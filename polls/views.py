501cd3bba63c7e8d2ac43933cbd01cf13ace0f27
leonardodamata <leonardodamata@gmail.com>
2022-05-04 16:15:33 -0300

    Use o sistema de template
    https://docs.djangoproject.com/en/4.0/intro/tutorial03/

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,17 @@
+from genericpath import exists
+from multiprocessing import context
+from re import template
+from unittest import loader
 from urllib import response
 from  django.http import HttpResponse
 from requests import request
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 
-def detail(request, questio_id):
-  return HttpResponse("Você está olhando para a pergunta %s." % questio_id)
+from .models import Question
+
+def detail(request, question_id):
+  return HttpResponse("Você está olhando para a pergunta %s." % question_id)
 
 def results(request, question_id) :
   response = "Você está vendo os resultados da pergunta  %s."
@@ -11,3 +19,12 @@ def results(request, question_id) :
 
 def vote(request, question_id):
   return HttpResponse("Você está votando na pergunta  %s." % question_id) 
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+  question = Question.objects.get(pk=question_id)
+  return render(request,'polls/detail.html', {'question': question})  
